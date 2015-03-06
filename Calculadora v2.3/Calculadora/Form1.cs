ea8c61b86fd99051b817213f8a514f18f1649ddc
ThiagoZx <TheThiagoo0@gmail.com>
2015-03-06 11:19:22 -0300

    Versão 2.3

---
+++ Calculadora v2.3/Calculadora/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Operation;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        //Sinaliza a vírgula
+        bool Dot_on = false;
+
+        //Diz o limite de digitos
+        int digit_limit = 19;
+
+        //vê a operação
+        string operation = "";
+        
+        //analisa e guarda o valor
+        double value = 0;
+        
+        //avisa quando dar o resultado
+        bool waiting_result = false;
+
+        public Operations calc = new Operations();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Operations calc = new Operations();
+        }
+
+        public void Button_Click(object sender, EventArgs e)
+        {
+            if (result.Text.Equals("0") || waiting_result) {
+                result.Text = null;
+                waiting_result = false;
+            }
+                
+            if (result.Text.Length < digit_limit) 
+            {
+                Button digit = sender as Button;
+                result.Text += digit.Text;
+            }
+        }
+
+        private void Dot_Click(object sender, EventArgs e)
+        {
+            Button digit = sender as Button;
+            if (!Dot_on) {
+                result.Text += digit.Text;
+                Dot_on = true;
+            }
+        }
+
+        public void Clear_Number(object sender, EventArgs e)
+        {
+            result.Text = "0";
+            Dot_on = false;
+        }
+
+        public void Erase_Digit(object sender, EventArgs e)
+        {
+            if (result.Text.ToString().IndexOf(",").Equals(result.Text.ToString().Length - 1)) { Dot_on = false; }
+            
+            result.Text = result.Text.ToString().Remove(result.Text.ToString().Length - 1);
+            
+            if (result.Text.ToString().Length <= 0) { result.Text = "0"; Dot_on = false; }
+        }
+        
+        private void Clear_all(object sender, EventArgs e)
+        {
+            value = 0;
+            result.Text = "0";
+            Dot_on = false;
+            waiting_result = false;
+            operation = "";
+        }
+
+        public void Operation_Click(object sender, EventArgs e)
+        {
+            Button digit = sender as Button;
+            operation = digit.Text;
+            value = Convert.ToDouble(result.Text);
+            waiting_result = true;
+            Dot_on = false;
+            Equation.Text = value + operation;
+        }
+
+        public void Quick_Operation(object sender, EventArgs e) 
+        {
+            Button digit = sender as Button;
+            operation = digit.Text;
+            value = Convert.ToDouble(result.Text);
+
+            switch (operation) { 
+
+                case "±":
+                    result.Text = calc.invrs(value);
+                    break;
+
+                case "√":
+                    result.Text = calc.sqrt(value);
+                    break;
+
+                case " ^ ":
+                    result.Text = calc.pow(value);
+                    break;
+
+                case "tg":
+                    result.Text = calc.tg(value);
+                    break;
+
+                case "cos":
+                    result.Text = calc.cos(value);
+                    break;
+
+                case "sen":
+                    result.Text = calc.sin(value);
+                    break;
+
+                default:
+                    break;
+            }
+            waiting_result = false;
+            if (Convert.ToDouble(result.Text) % 1 != 0)
+            {
+                Dot_on = true;
+            }
+        }
+
+        public void Check_Answer(object sender, EventArgs e)
+        {
+            if (operation == "") { return ; }
+            Equation.Text = null;
+            switch (operation) { 
+                case " + ":
+                    result.Text = calc.soma(value, Convert.ToDouble(result.Text));
+                    break;
+
+                case " - ":
+                    result.Text = calc.subtr(value, Convert.ToDouble(result.Text));
+                    break;
+
+                case " * ":
+                    result.Text = calc.multp(value, Convert.ToDouble(result.Text));
+                    break;
+                
+                case " / ":
+                    result.Text = calc.div(value, Convert.ToDouble(result.Text));
+                    break;
+
+                case " R ":
+                    result.Text = calc.rst(value, Convert.ToDouble(result.Text));
+                    break;
+
+                case " % ":
+                    result.Text = calc.pct(value, Convert.ToDouble(result.Text));
+                    break;
+
+                default:
+                    break;
+            }
+            waiting_result = false;
+            if (Convert.ToDouble(result.Text) % 1 == 0) {
+                Dot_on = false;
+            }
+        }
+
+
+    }
+}
---
+++ Calculadora v2.3/Calculadora/Operations.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Calculadora;
+
+namespace Operation
+{
+    public partial class Operations : Form
+    {
+        public string soma(double a, double b) {
+            double c = a + b;
+            return Convert.ToString(c);
+        }
+
+        public string subtr(double a, double b)
+        {
+            double c = a - b;
+            return Convert.ToString(c);
+        }
+
+        public string multp(double a, double b)
+        {
+            double c = a * b;
+            return Convert.ToString(c);
+        }
+
+        public string div(double a, double b)
+        {
+            double c = a / b;
+            return Convert.ToString(c);
+        }
+        
+        public string rst(double a, double b)
+        {
+            double c = a % b;
+            return Convert.ToString(c);
+        }
+
+        public string pct(double a, double b)
+        {
+            double c = a / 100 * b;
+            return Convert.ToString(c);
+        }
+
+        public string invrs(double a)
+        {
+            double b = a * (-1);
+            return Convert.ToString(b);
+        }
+
+        public string sqrt(double a)
+        {
+            double b = Math.Sqrt(a);
+            return Convert.ToString(b);
+        }
+
+        public string pow(double a)
+        {
+            double b = Math.Pow(a, 2);
+            return Convert.ToString(b);
+        }
+
+        public string sin(double a)
+        {
+            double b = Math.Sin(a);
+            return Convert.ToString(b);
+        }
+
+        public string cos(double a)
+        {
+            double b = Math.Cos(a);
+            return Convert.ToString(b);
+        }
+
+        public string tg(double a)
+        {
+            double b = Math.Tan(a);
+            return Convert.ToString(b);
+        }
+    }
+}
